091cf8b95e7b68e8d87915ba9346c551f8c08137
Mikhail Sokolov <m_sokoloff@mail.ru>
2020-08-04 11:42:04 +0400

    Добавлена домашняя работа 1-Performance-Profiler

---
+++ 1-Performance-Profiler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_Performance_Profiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "a";
+            while (true)
+            {
+                myString += myString;
+                Console.WriteLine(myString.Length);
+            }
+            
+        }
+    }
+}

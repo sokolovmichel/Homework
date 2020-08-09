59e9d75ff9951c89e5c121b3e032b40211b80020
Mikhail Sokolov <m_sokoloff@mail.ru>
2020-08-09 11:02:07 +0400

    solution перенесен в отдельную папку

---
+++ HomeworkSLN/Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Homework;
+
+
+namespace FormattingCoordinates
+{
+    /// <summary>
+    /// Точка входа
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ReadData.Read();
+            ReadData.Convert(s);
+            Console.Read();
+        }
+    }
+
+    
+}
--- HomeworkSLN/Homework/ReadData.cs
+++ HomeworkSLN/Homework/ReadData.cs
@@ -1,27 +1,14 @@
 ﻿using System;
 using System.Globalization;
 
-
-namespace FormattingCoordinates
+namespace Homework
 {
-    /// <summary>
-    /// Точка входа
-    /// </summary>
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            string s = ReadData.Read();
-            ReadData.Convert(s);
-            Console.Read();
-        }
-    }
 
     /// <summary>
     /// Класс читает координаты из перенаправленного файла и выводит их
     /// в отформатированной виде на консоль
     /// </summary>
-    public class ReadData
+    class ReadData
     {
         /// <summary>
         /// метод читает читает координаты из перенаправленного файла
@@ -30,14 +17,13 @@ namespace FormattingCoordinates
         /// <returns></returns>
         public static string Read()
         {
-            var AllText = String.Empty;
+            var AllText = string.Empty;
 
             try
             {
-                string line = String.Empty;
+                string line = string.Empty;
                 do
                 {
-                    
                     line = Console.ReadLine();
                     AllText += line + ",";
 
@@ -45,14 +31,14 @@ namespace FormattingCoordinates
 
                 return AllText;
             }
-          
+
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
             return null;
-                        
+
         }
 
         /// <summary>
@@ -62,11 +48,11 @@ namespace FormattingCoordinates
 
         public static string Convert(string AllText)
         {
-            Decimal[] X, Y;
+            decimal[] X, Y;
 
-            string Text = String.Empty;
+            string Text = string.Empty;
 
-            Char[] Separator = { '\t', '\r', '\n', ' ', ',' };
+            char[] Separator = { '\t', '\r', '\n', ' ', ',' };
 
             var Coordinates = AllText.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
             var n = Coordinates.Length;
@@ -74,51 +60,41 @@ namespace FormattingCoordinates
             if (z != 0)
             {
                 Console.WriteLine("Количество исходных данных не кратно двум");
-                
+
                 return null;
             }
 
+            X = new decimal[n / 2];
+            Y = new decimal[n / 2];
 
-
-            X = new Decimal[n / 2]; Y = new Decimal[n / 2];
-
-            Boolean A, B;
+            bool A, B;
             var j = 0;
+
 
             for (var i = 0; i <= n / 2 - 1; i++)
             {
-
                 IFormatProvider culture = new CultureInfo("en-Us", useUserOverride: true);
-                A = Decimal.TryParse(Coordinates[j], NumberStyles.AllowDecimalPoint, culture, out X[i]);
+                A = decimal.TryParse(Coordinates[j], NumberStyles.AllowDecimalPoint, culture, out X[i]);
                 j = j + 1;
-                B = Decimal.TryParse(Coordinates[j], NumberStyles.AllowDecimalPoint, culture, out Y[i]);
+                B = decimal.TryParse(Coordinates[j], NumberStyles.AllowDecimalPoint, culture, out Y[i]);
                 j = j + 1;
 
                 if ((A && B) == false)
-                    Console.WriteLine(String.Format("В строке {0} - не числовой ввод!", i + 1));
-
+                    Console.WriteLine(string.Format("В строке {0} - не числовой ввод!", i + 1));
             }
 
             for (var i = 0; i <= n / 2 - 1; i++)
             {
-                Console.Write(String.Format("X: {0,-7} " + "Y: {1,-7}" + "\n",
+                IFormatProvider culture = CultureInfo.CurrentCulture;
+                Console.Write(string.Format(culture, "X: {0,-7} " + "Y: {1,-7}" + "\n",
                     X[i], Y[i]));
 
-               
-                
-                Text = String.Format("X: {0}" + "Y: {1}", X[i], Y[i]);
-
-                 
+                Text = string.Format("X: {0}" + "Y: {1}", X[i], Y[i]);
 
             }
 
-           
-
             return Text;
-            
 
         }
-
-
     }
 }

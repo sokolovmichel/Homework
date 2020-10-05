f06fa9df32bfd3fa662ac8adac83758e2c597afd
Mikhail Sokolov <m_sokoloff@mail.ru>
2020-10-05 21:17:27 +0400

    Контрольная работа №2

---
+++ HomeworkSLN/Test2/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    public static class Extension
+    {
+        public static string GetElement(this List<string> list, int i)
+        {
+            string myString = list.ElementAt(i);
+            return myString;
+
+        }
+    }
+}
---
+++ HomeworkSLN/Test2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>() { "one", "two", "three", "four", "five" };
+            string myString = myList.GetElement(1);
+            Console.WriteLine(myString);
+
+            Console.WriteLine();
+
+            string str = "seven";
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
+            Search(path, str);
+
+            
+
+            
+
+        }
+
+        static void Search(string path, string str)
+        {
+            
+            string[] directories = Directory.GetDirectories(path);
+            
+
+            foreach (var d in directories)
+            {
+                string[] files = Directory.GetFiles(d, "*.txt");
+                foreach (var f in files)
+                {
+                    var Reader = new StreamReader(f);
+                    string text = Reader.ReadToEnd();
+                    char[] Separator = { '\t', '\r', '\n', ' ', ',' };
+
+                    var newText = text.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+
+                    int i = newText.Where(t => t.Equals(str)).Count();
+                  
+                   
+                    if (newText.Contains(str))
+                    {
+                        //Console.WriteLine(f);
+                        Console.WriteLine(i);
+
+                        FileInfo fi = new FileInfo(f);
+                        Console.WriteLine(fi.Length);
+                        Console.WriteLine(fi.Name);
+                        Console.WriteLine(fi.DirectoryName);
+
+                    }
+                    
+                   
+                }
+                Search(d, str);
+
+            }
+        }
+    }
+}
